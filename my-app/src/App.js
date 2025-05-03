e53c3c63a2a5e8cbf7f7ab3ae4e4f4c4d080f5df
Madiha Sulthana <madihasulthana08@gmail.com>
2025-05-03 11:40:53 +0530

    This is new file with react app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>This is my Madiha</h1>
       <p>Welcome to my React app.</p>
     </div>
   );
